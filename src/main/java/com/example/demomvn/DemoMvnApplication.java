1b576fae03bb529ce4060d537c8c34c3596bb6e2
satya <sahoo.satyabrata36@gmail.com>
2023-11-25 11:21:05 +0530

    added new repo

---
+++ src/main/java/com/example/demomvn/DemoMvnApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demomvn/DemoMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
